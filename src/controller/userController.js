2ff5cf2ea2515de95839d795731290ffbb25837b
breadsticks64 <alif.anke.bima@gmail.com>
2023-03-28 01:15:43 +0700

    fixed bug in uploading user image

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -158,9 +158,8 @@ const userController = {
       }
 
       const result = await userModel.findId(userId);
-      
       // Update image if image already exists in database
-      if (req.file && result.rows[0].image != undefined) {
+      if (req.file && result.rows[0].image != null) {
         const oldImage = result.rows[0].image;
         const oldImageId = oldImage.split("=")[1];
         const updateResult = await googleDrive.updateImage(req.file, oldImageId)
@@ -168,14 +167,14 @@ const userController = {
         image = parentPath.concat(updateResult.id)
 
       // Upload image if image doesn't exists in database
-      } else if (req.file && result.rowCount == 0) {
+      } else if (req.file && result.rows[0].image == null) {
         const uploadResult = await googleDrive.uploadImage(req.file)
         const parentPath = process.env.GOOGLE_DRIVE_PHOTO_PATH;
         image = parentPath.concat(uploadResult.id)
       }
 
       // Use previous image if user doesn't upload new image
-      else if (result.rows[0].image != undefined) {
+      else {
         image = result.rows[0].image;
       }
 
@@ -198,7 +197,7 @@ const userController = {
       const dataPw = await userModel.findId(id);
 
       const updatedData = {
-        name: newData.fullname || dataPw.rows[0].name,
+        fullname: newData.fullname || dataPw.rows[0].fullname,
         email: newData.email || dataPw.rows[0].email,
         password: newData.password || dataPw.rows[0].password,
         image: image || dataPw.rows[0].image,
@@ -209,7 +208,7 @@ const userController = {
       };
 
       await userModel.editProfile(
-        updatedData.name,
+        updatedData.fullname,
         updatedData.email,
         updatedData.password,
         updatedData.image,
